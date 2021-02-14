98000e62bc8c72c13ee63438880233a05109c17d
Nicolas Beaussart <nic.beaussart@gmail.com>
2021-02-14 20:15:15 +0100

    :sparkles: update dependancies and improve code

--- codelab-watcher.js
+++ codelab-watcher.js
@@ -6,18 +6,10 @@ const util = require('util');
 
 const cp = util.promisify(ncpCallback);
 
-function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  }
-
 const buildApp = async () => {
   const buildOra = ora('Building codelab').start();
   await builder('./src/codelab.md', 'tmpLab');
-  // Dirty hack, waiting for a way to await the end of the generation
-  // see https://github.com/DX-DeveloperExperience/codelab-generator/pull/16
-  await sleep(1000)
+
   buildOra.succeed('Codelab generated');
   const copyOra = ora('Copy images').start();
   await cp('./src/img/', 'tmpLab/img/');
--- src/slides.md
+++ src/slides.md
@@ -24,3 +24,10 @@
 
 
 ![gitbranch](img/simple-git-flow.jpg)
+
+
+---
+
+# Practice time !
+
+[Codelab !](./lab)
